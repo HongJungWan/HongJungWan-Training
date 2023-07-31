a22a324d88abce4ed14a9cd5664029fa15fa42c1
HongJungWan <hjw43ok@hs.ac.kr>
2023-07-31 12:54:19 +0900

    feat: 뇌 단련 #5

---
+++ src/main/java/com/test/training/next_step/level_2/Pg_0731_1.java
@@ -0,0 +1,40 @@
+package com.test.training.next_step.level_2;
+
+/**
+ * level 2 - 다음 큰 숫자
+ */
+public class Pg_0731_1 {
+    public static void main(String[] args) {
+        System.out.println(solution(15));
+    }
+
+    public static int solution(int n) {
+        int tempOne = 0;
+        int tempTwo = 0;
+        int answer = 0;
+        String binaryString = "";
+
+        binaryString = Integer.toBinaryString(n);
+        for (char c : binaryString.toCharArray()) {
+            if (c == '1') {
+                tempOne++;
+            }
+        }
+
+        while (true) {
+            n++;
+            tempTwo = 0;
+            binaryString = Integer.toBinaryString(n);
+            for (char c : binaryString.toCharArray()) {
+                if (c == '1') {
+                    tempTwo++;
+                }
+            }
+            if (tempOne == tempTwo) {
+                answer = n;
+                break;
+            }
+        }
+        return answer;
+    }
+}
