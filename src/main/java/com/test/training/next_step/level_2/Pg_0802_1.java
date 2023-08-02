f5dd197b51b6e83a4ef7749e4860622758c5208e
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2023-08-02 20:14:30 +0900

    feat: 뇌 단련 #7 (#9)

---
+++ src/main/java/com/test/training/next_step/level_2/Pg_0802_1.java
@@ -0,0 +1,26 @@
+package com.test.training.next_step.level_2;
+
+import java.util.Stack;
+
+/**
+ * level 2 - 짝지어 제거하기
+ */
+public class Pg_0802_1 {
+    public static void main(String[] args) {
+        System.out.println(solution("baabaa"));
+        System.out.println(solution("cdcd"));
+    }
+
+    public static int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
