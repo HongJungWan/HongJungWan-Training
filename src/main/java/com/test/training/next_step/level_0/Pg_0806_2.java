37d3b0017d8508ffea936d9f919971bae40e7637
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2023-08-06 16:10:20 +0900

    feat: 뇌 단련 #9 (#11)

---
+++ src/main/java/com/test/training/next_step/level_0/Pg_0806_2.java
@@ -0,0 +1,24 @@
+package com.test.training.next_step.level_0;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * level 0 - n 번째 원소부터
+ */
+public class Pg_0806_2 {
+    public static void main(String[] args) {
+        int[] num_list = {2, 1, 6};
+        System.out.println(solution(num_list, 3));
+    }
+
+    public static int[] solution(int[] num_list, int n) {
+        List list = new ArrayList<>();
+        for (int i = n - 1; i < num_list.length; i++) {
+            list.add(num_list[i]);
+        }
+        return list.stream()
+                .mapToInt(e -> (int) e)
+                .toArray();
+    }
+}
