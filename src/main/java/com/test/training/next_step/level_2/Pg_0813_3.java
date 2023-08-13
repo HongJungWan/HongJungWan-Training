6b3acf285b65917b80938884f0c3360018823523
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2023-08-13 12:10:19 +0900

    feat: 뇌 단련 #14 (#16)

---
+++ src/main/java/com/test/training/next_step/level_2/Pg_0813_3.java
@@ -0,0 +1,33 @@
+package com.test.training.next_step.level_2;
+
+import java.util.Arrays;
+
+/**
+ * level 2 - 구명보트
+ */
+public class Pg_0813_3 {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{70, 50, 80, 50}, 100));
+        System.out.println(solution(new int[]{70, 80, 50}, 100));
+    }
+
+    public static int solution(int[] people, int limit) {
+        int answer = 0;
+        int left = 0;
+        int right = people.length - 1;
+
+        Arrays.sort(people);
+
+        while (left <= right) {
+            if ((people[left] + people[right]) <= limit) {
+                left++;
+                right--;
+                answer++;
+            } else {
+                right--;
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
