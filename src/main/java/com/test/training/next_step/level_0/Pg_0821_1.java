136886284f5adf46487469021d83b8198ef58eeb
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2023-08-21 10:40:53 +0900

    feat: 뇌 단련 #19 (#21)

---
+++ src/main/java/com/test/training/next_step/level_0/Pg_0821_1.java
@@ -0,0 +1,16 @@
+package com.test.training.next_step.level_0;
+
+/**
+ * level 0 - 부분 문자열인지 확인하기
+ */
+public class Pg_0821_1 {
+    public static void main(String[] args) {
+        System.out.println(solution("banana", "ana"));
+        System.out.println(solution("banana", "wxyz"));
+    }
+
+    public static int solution(String my_string, String target) {
+        if (my_string.contains(target)) return 1;
+        return 0;
+    }
+}
