d8286a35c5a25a63b1ac41ecd47ac9c2496de1d4
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2023-08-24 21:31:29 +0900

    feat: 뇌 단련 #20

---
+++ src/main/java/com/test/training/next_step/level_0/Pg_0824_3.java
@@ -0,0 +1,16 @@
+package com.test.training.next_step.level_0;
+
+/**
+ * level 0 - 원하는 문자열 찾기
+ */
+public class Pg_0824_3 {
+    public static void main(String[] args) {
+        System.out.println(solution("AbCdEfG", "aBc"));
+        System.out.println(solution("aaAA", "aaaaa"));
+    }
+
+    public static int solution(String myString, String pat) {
+        if (myString.toLowerCase().contains(pat.toLowerCase())) return 1;
+        return 0;
+    }
+}
