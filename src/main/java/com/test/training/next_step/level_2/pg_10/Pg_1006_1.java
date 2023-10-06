74581f8ebbdeb6fc83a86d2f6b7ecc191d6338f6
HongJungWan <hjw43ok@hs.ac.kr>
2023-10-06 10:44:41 +0900

    feat: 뇌 단련 #50

--- src/main/java/com/test/training/next_step/level_2/pg_10/Pg_1006_1.java
+++ src/main/java/com/test/training/next_step/level_2/pg_10/Pg_1006_1.java
@@ -1,9 +1,9 @@
 package com.test.training.next_step.level_2.pg_10;
 
 /**
- * level 2 - 주식가격
+ * level 2 - 주식가격 (재도전)
  */
-public class Pg_1004_1 {
+public class Pg_1006_1 {
     public static void main(String[] args) {
         for (int answer : solution(new int[]{1, 2, 3, 2, 3})) {
             System.out.print(answer + " ");
