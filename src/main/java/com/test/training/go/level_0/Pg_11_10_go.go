459f6bc6c05a46ccc857675812e5368528d40e17
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2023-11-13 20:30:43 +0900

    feat: 뇌 단련 #60

--- src/main/java/com/test/training/go/level_0/Pg_11_10_go.go
+++ src/main/java/com/test/training/go/level_0/Pg_11_10_go.go
@@ -5,12 +5,12 @@ import (
 	"strings"
 )
 
-func solution(n_str string) string {
+func Pg_11_10(n_str string) string {
 	// strings.TrimLeft 함수를 사용하여 왼쪽의 "0"들을 제거한다.
 	return strings.TrimLeft(n_str, "0")
 }
 
 func main() {
-	fmt.Println(solution("0010"))   // "10"
-	fmt.Println(solution("854020")) // "854020"
+	fmt.Println(Pg_11_10("0010"))   // "10"
+	fmt.Println(Pg_11_10("854020")) // "854020"
 }
