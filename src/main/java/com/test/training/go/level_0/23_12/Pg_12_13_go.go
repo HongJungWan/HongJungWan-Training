f08f8cb8110ec4dfd533b32a59d521fc08d9a1c8
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2023-12-13 20:24:19 +0900

    feat: 뇌 단련 #85 (#85)

---
+++ src/main/java/com/test/training/go/level_0/23_12/Pg_12_13_go.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Pg_12_13(arr []int, queries [][]int) []int {
+	n := len(arr)
+	window := make([]int, n)
+
+	for _, query := range queries {
+		s, e := query[0], query[1]
+		window[s]++
+		if e+1 < n {
+			window[e+1]--
+		}
+	}
+
+	sum := 0
+	for i := 0; i < n; i++ {
+		sum += window[i]
+		arr[i] += sum
+	}
+	return arr
+}
+
+func main() {
+	arr := []int{0, 1, 2, 3, 4}
+	queries := [][]int{{0, 1}, {1, 2}, {2, 3}}
+
+	fmt.Println(Pg_12_13(arr, queries)) // Output: [1, 3, 4, 4, 4]
+}
+
+// Sliding Window
