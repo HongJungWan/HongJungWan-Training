c87a9e04e4ff9f997d70d5ef2e18a1caca81cfb2
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2024-02-21 20:29:04 +0900

    feat: 뇌 단련 #139 (#139)

--- src/main/java/com/test/training/go/core/hash/HASH_BG_9612.go
+++ src/main/java/com/test/training/go/core/hash/HASH_BG_9612.go
@@ -22,7 +22,10 @@ func readWords(n int) map[string]int {
 func findMostFrequentWord(wordFreq map[string]int) (string, int) {
 	var maxFreq int
 	var mostFreqWord string
+
+	// map을 range 문과 함께 사용할 때, 키(key)와 해당 키에 연결된 값(value)을 반환
 	for word, freq := range wordFreq {
+		// 두번째 조건, 현재 단어의 빈도가 최대 빈도와 같고, 현재 단어가 알파벳 순으로 지금까지 발견된 가장 빈도가 높은 단어
 		if freq > maxFreq || (freq == maxFreq && word > mostFreqWord) {
 			mostFreqWord = word
 			maxFreq = freq
