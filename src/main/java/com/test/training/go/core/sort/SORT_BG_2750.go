4d22dd3f54e1e77fae2edca4e2253e0545b8957b
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2024-03-04 23:04:02 +0900

    feat: 뇌 단련 #147 (#147)

---
+++ src/main/java/com/test/training/go/core/sort/SORT_BG_2750.go
@@ -0,0 +1,32 @@
+// BG - 수 정렬하기
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var N int
+	fmt.Fscanln(reader, &N) // N 개수 입력 받기
+
+	numbers := make([]int, N)
+	for i := 0; i < N; i++ {
+		fmt.Fscanln(reader, &numbers[i])
+	}
+
+	sort.Slice(numbers, func(i, j int) bool {
+		return numbers[i] < numbers[j]
+	})
+
+	for _, number := range numbers {
+		fmt.Fprintln(writer, number)
+	}
+}
