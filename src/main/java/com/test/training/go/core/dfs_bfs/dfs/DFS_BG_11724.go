d5c40f80b32b676ab7d4a8b7a7ee7b5879f8e051
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2024-03-06 23:59:36 +0900

    feat: 뇌 단련 #151 (#151)

--- src/main/java/com/test/training/go/core/dfs_bfs/dfs/DFS_BG_11724.go
+++ src/main/java/com/test/training/go/core/dfs_bfs/dfs/DFS_BG_11724.go
@@ -32,7 +32,7 @@ func main() {
 	componentCount := 0
 	for node := 1; node <= numNodes; node++ {
 		if !visitedNode[node] {
-			exploreNode(node)
+			exploreNodeDFS(node)
 			componentCount++ // 새 연결 요소를 찾을 때마다 카운트 증가
 		}
 	}
@@ -41,15 +41,15 @@ func main() {
 	fmt.Println(componentCount)
 }
 
-// exploreNode 함수는 주어진 노드를 시작점으로 깊이 우선 탐색을 수행
-func exploreNode(node int) {
+// exploreNodeDFS 함수는 주어진 노드를 시작점으로 깊이 우선 탐색을 수행
+func exploreNodeDFS(node int) {
 	visitedNode[node] = true // 현재 노드를 방문했음을 표시
 
 	// 모든 노드에 대해 반복
 	for adjacentNode := 1; adjacentNode <= numNodes; adjacentNode++ {
 		// 현재 노드와 인접하며 아직 방문하지 않은 노드가 있다면
 		if adjMatrix[node][adjacentNode] && !visitedNode[adjacentNode] {
-			exploreNode(adjacentNode) // 인접 노드에서 탐색을 계속
+			exploreNodeDFS(adjacentNode) // 인접 노드에서 탐색을 계속
 		}
 	}
 }
