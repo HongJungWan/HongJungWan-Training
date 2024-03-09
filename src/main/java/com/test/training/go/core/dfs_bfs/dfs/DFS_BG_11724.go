775fe36f92cc7eaccdea6e26ccd43c495ac55581
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2024-03-09 12:59:30 +0900

    feat: 뇌 단련 #152 (#152)

--- src/main/java/com/test/training/go/core/dfs_bfs/dfs/DFS_BG_11724.go
+++ src/main/java/com/test/training/go/core/dfs_bfs/dfs/DFS_BG_11724.go
@@ -1,31 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 var (
-	adjMatrix          [][]bool // 인접 행렬로 그래프 표현
-	visitedNode        []bool   // 각 정점의 방문 여부
-	numNodes, numEdges int      // numNodes는 정점의 개수, numEdges는 간선의 개수
+	adjList            [][]int // 인접 리스트로 그래프 표현
+	visitedNode        []bool  // 각 정점의 방문 여부
+	numNodes, numEdges int     // 정점의 개수와 간선의 개수
 )
 
 func main() {
-	fmt.Scan(&numNodes, &numEdges) // 정점과 간선의 개수를 입력
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Fscan(reader, &numNodes, &numEdges)
 
-	// 인접 행렬과 방문 배열을 초기화
-	adjMatrix = make([][]bool, numNodes+1)
-	for i := range adjMatrix {
-		adjMatrix[i] = make([]bool, numNodes+1)
-	}
+	// 인접 리스트와 방문 배열을 초기화
+	adjList = make([][]int, numNodes+1)
 	visitedNode = make([]bool, numNodes+1)
 
-	// 간선 정보를 입력받아 인접 행렬에 저장
+	// 간선 정보를 입력받아 인접 리스트에 저장
 	var node1, node2 int
 	for i := 0; i < numEdges; i++ {
-		fmt.Scan(&node1, &node2)
-		adjMatrix[node1][node2] = true
-		adjMatrix[node2][node1] = true // 무방향 그래프이므로 양방향 표시
+		fmt.Fscan(reader, &node1, &node2)
+		adjList[node1] = append(adjList[node1], node2)
+		adjList[node2] = append(adjList[node2], node1) // 무방향 그래프이므로 양방향 추가
 	}
 
 	// 연결 요소의 개수를 구함
@@ -33,23 +33,34 @@ func main() {
 	for node := 1; node <= numNodes; node++ {
 		if !visitedNode[node] {
 			exploreNodeDFS(node)
-			componentCount++ // 새 연결 요소를 찾을 때마다 카운트 증가
+			componentCount++
 		}
 	}
 
 	// 연결 요소의 개수 출력
-	fmt.Println(componentCount)
+	writer := bufio.NewWriter(os.Stdout)
+	fmt.Fprintln(writer, componentCount)
+	writer.Flush()
 }
 
 // exploreNodeDFS 함수는 주어진 노드를 시작점으로 깊이 우선 탐색을 수행
 func exploreNodeDFS(node int) {
-	visitedNode[node] = true // 현재 노드를 방문했음을 표시
+	visitedNode[node] = true
 
-	// 모든 노드에 대해 반복
-	for adjacentNode := 1; adjacentNode <= numNodes; adjacentNode++ {
-		// 현재 노드와 인접하며 아직 방문하지 않은 노드가 있다면
-		if adjMatrix[node][adjacentNode] && !visitedNode[adjacentNode] {
-			exploreNodeDFS(adjacentNode) // 인접 노드에서 탐색을 계속
+	// 인접 리스트를 사용하여 인접 노드 반복
+	for _, adjacentNode := range adjList[node] {
+		if !visitedNode[adjacentNode] {
+			exploreNodeDFS(adjacentNode)
 		}
 	}
 }
+
+// 인접 행렬을 사용할 경우, 간선의 조회에 O(n^2)의 시간이 소요된다. 이는 각 정점 쌍에 대해 연결 여부를 확인해야 하기 때문이다.
+// 정점의 수(n)가 많을 때, 이 방법은 매우 비효율적이 될 수 있다.
+// 특히, 간선의 수(e)가 정점의 수에 비해 상대적으로 적은 "희소 그래프(sparse graph)"의 경우 이러한 비효율성은 더욱 두드러진다.
+
+// 인접 리스트를 사용하는 방법은 이러한 문제를 해결할 수 있는 방법이다.
+// 인접 리스트는 각 정점에서 직접 연결된 간선들만을 리스트로 관리하여, 간선의 조회 시간을 O(V+E)로 줄일 수 있다.
+// 여기서 V는 정점의 수, E는 간선의 수를 의미한다.
+// 이 방식은 각 정점에서 출발하는 간선들을 바로 접근할 수 있기 때문에, 그래프 탐색과 같은 작업을 보다 효율적으로 수행할 수 있다.
+// 특히, 희소 그래프에서 각 정점에 연결된 간선의 수가 적기 때문에, 전체 작업의 시간 복잡도가 상당히 감소한다.
