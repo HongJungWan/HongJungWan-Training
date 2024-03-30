0b852acf43c5c10ad8bbdbcab06cc0442510e9b6
HONG-JUNG-WAN <hong43ok@gmail.com>
2024-03-30 19:25:10 +0900

    feat: 뇌 단련 #171

--- src/main/java/com/test/training/go/core/stack/STACK_BG_27969.go
+++ src/main/java/com/test/training/go/core/stack/STACK_BG_27969.go
@@ -10,56 +10,40 @@ import (
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	input, _ := reader.ReadString('\n')
-	fmt.Println(calculateASONSize(input))
+	fmt.Println(parseASON(input))
 }
 
-// calculateASONSize 함수는 전체 ASON 객체의 크기를 계산합
-func calculateASONSize(ason string) int {
-	size := 8 // 시작 ASON 객체의 기본 크기
-	i := 0    // 인덱스 초기화
+func parseASON(ason string) int {
+	var stack []int
+	size := 0
 
-	for i < len(ason) {
-		if ason[i] == '[' {
-			// 새 ASON 객체가 시작되었을 때
+	for i := 0; i < len(ason); i++ {
+		char := ason[i]
+
+		switch char {
+		case '[':
+			stack = append(stack, size)
+			size = 0
+		case ']':
 			size += 8
-			i++
-		} else if ason[i] == ']' {
-			// ASON 객체가 끝났을 때
-			i++
-		} else {
-			// 다음 원소 찾기
-			element, nextIndex := getNextElement(ason, i)
-			size += calculateElementSize(element)
-			i = nextIndex
+			if len(stack) > 0 {
+				size += stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+			}
+		default:
+			if unicode.IsDigit(rune(char)) {
+				for i+1 < len(ason) && unicode.IsDigit(rune(ason[i+1])) {
+					i++
+				}
+				size += 8
+			} else if unicode.IsLetter(rune(char)) {
+				start := i
+				for i+1 < len(ason) && unicode.IsLetter(rune(ason[i+1])) {
+					i++
+				}
+				size += (i - start + 1) + 12
+			}
 		}
 	}
 	return size
-}
-
-// getNextElement 함수는 주어진 문자열에서 다음 원소와 그 이후 인덱스를 반환
-func getNextElement(ason string, start int) (element string, nextIndex int) {
-	for nextIndex = start; nextIndex < len(ason); nextIndex++ {
-		if unicode.IsSpace(rune(ason[nextIndex])) {
-			if nextIndex > start {
-				break
-			}
-			continue
-		}
-	}
-	element = ason[start:nextIndex]
-	return element, nextIndex
-}
-
-// calculateElementSize 함수는 원소의 유형에 따라 크기를 계산
-func calculateElementSize(element string) int {
-	if len(element) == 0 { // 빈 원소
-		return 0
-	}
-	if unicode.IsDigit(rune(element[0])) { // 양의 정수 자료형
-		return 8
-	}
-	if unicode.IsLetter(rune(element[0])) { // 문자열 자료형
-		return len(element) + 12
-	}
-	return 0 // 다른 유형의 원소는 0으로 가정
 }
