d26c2fdf78bf5799e70b002d17026987195367fa
HONG-JUNG-WAN <hong43ok@gmail.com>
2024-03-31 15:01:08 +0900

    feat: 뇌 단련 #172

---
+++ src/main/java/com/test/training/go/core/dfs/DFS_BG_2644.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n, m, person1, person2 int
+	fmt.Scanf("%d\n%d %d\n%d", &n, &person1, &person2, &m)
+
+	graph := make(map[int][]int)
+	visited := make(map[int]bool)
+
+	for i := 0; i < m; i++ {
+		var parent, child int
+		fmt.Scanf("%d %d", &parent, &child)
+		graph[parent] = append(graph[parent], child)
+		graph[child] = append(graph[child], parent) // 양방향으로 추가
+	}
+	result := calculateKinship(graph, visited, person1, person2, 0)
+	fmt.Println(result)
+}
+
+func calculateKinship(graph map[int][]int, visited map[int]bool, start, end, depth int) int {
+	if start == end {
+		return depth
+	}
+
+	visited[start] = true
+	for _, next := range graph[start] {
+		if !visited[next] {
+			distance := calculateKinship(graph, visited, next, end, depth+1)
+			if distance != -1 {
+				return distance
+			}
+		}
+	}
+	return -1
+}
