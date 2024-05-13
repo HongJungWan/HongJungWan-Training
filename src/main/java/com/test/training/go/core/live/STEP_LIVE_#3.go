012b19eba543b5a942c7238bdf820881f90812cc
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2024-05-14 08:54:52 +0900

    feat: 뇌 단련 #201 (#201)
    Co-authored-by: jungwanhong-metanet <jungwan.hong.metanet@joyent.com>

--- src/main/java/com/test/training/go/core/live/STEP_LIVE_#3.go
+++ src/main/java/com/test/training/go/core/live/STEP_LIVE_#3.go
@@ -1,3 +1,44 @@
 // TODO: 버블 정렬 (Bubble Sort)
+// TODO: 서로 인접한 두 원소를 검사하여 정렬
 
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	input := scanner.Text()
+
+	bubbleSortItems := strings.Split(input, " ")
+	numbers := make([]int, len(bubbleSortItems))
+
+	for i, s := range bubbleSortItems {
+		number, err := strconv.Atoi(s)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+			return
+		}
+		numbers[i] = number
+	}
+
+	bubbleSort(numbers)
+	fmt.Println("Sorted numbers:", numbers)
+}
+
+func bubbleSort(arr []int) {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+}
