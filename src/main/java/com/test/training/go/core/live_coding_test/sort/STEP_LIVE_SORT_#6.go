15fcbdce74faf5fc0cf2545ac9349a2ff5cc2465
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2024-05-17 13:01:58 +0900

    feat: 뇌 단련 #205 (#204)
    Co-authored-by: jungwanhong-metanet <jungwan.hong.metanet@joyent.com>

--- src/main/java/com/test/training/go/core/live_coding_test/sort/STEP_LIVE_SORT_#6.go
+++ src/main/java/com/test/training/go/core/live_coding_test/sort/STEP_LIVE_SORT_#6.go
@@ -1,6 +1,3 @@
-// TODO: 퀵 정렬 (Quick Sort)
-// TODO: 피벗을 기준으로 배열을 분할하여 피벗보다 작은 요소와 큰 요소로 나눈 뒤, 이 과정을 재귀적으로 반복해 정렬
-
 package main
 
 import (
@@ -34,17 +31,20 @@ func main() {
 
 func quickSort(arr []int, low int, high int) {
 	if low < high {
-		// pi는 파티셔닝 인덱스, arr[pi]는 이제 정확한 위치에 있다.
-		pi := partition(arr, low, high)
+		// 분할 (Divide)
+		partitionIndex := partition(arr, low, high)
 
-		// 별도로 pi 전후의 요소들을 재귀적으로 정렬
-		quickSort(arr, low, pi-1)
-		quickSort(arr, pi+1, high)
+		// 정복 (Conquer)
+		quickSort(arr, low, partitionIndex-1)
+		quickSort(arr, partitionIndex+1, high)
 	}
 }
 
 func partition(arr []int, low int, high int) int {
-	pivot := arr[high] // 피벗
+	mid := low + (high-low)/2                 // 중간 인덱스 계산
+	arr[mid], arr[high] = arr[high], arr[mid] // 중간 값을 끝으로 이동
+
+	pivot := arr[high] // 이제 중간 값이 피벗
 	i := low - 1       // 가장 작은 요소의 인덱스
 
 	for j := low; j < high; j++ {
@@ -54,6 +54,6 @@ func partition(arr []int, low int, high int) int {
 			arr[i], arr[j] = arr[j], arr[i]
 		}
 	}
-	arr[i+1], arr[high] = arr[high], arr[i+1]
+	arr[i+1], arr[high] = arr[high], arr[i+1] // 피벗을 올바른 위치로 교환
 	return i + 1
 }
