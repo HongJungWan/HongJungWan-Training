d15e05a1ea61507666b0fceb8d5696ac7b5151f0
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2024-05-17 12:22:17 +0900

    feat: 뇌 단련 #203 (#203)
    Co-authored-by: jungwanhong-metanet <jungwan.hong.metanet@joyent.com>

--- src/main/java/com/test/training/go/core/live_coding_test/factorial/STEP_LIVE_FACTORIAL_#12.go
+++ src/main/java/com/test/training/go/core/live_coding_test/factorial/STEP_LIVE_FACTORIAL_#12.go
@@ -1,3 +1,34 @@
 // TODO: 팩토리얼
+// TODO: 팩토리얼은 주어진 숫자부터 1까지의 모든 정수의 곱
 
-package factorial
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	input := scanner.Text()
+
+	n, _ := strconv.Atoi(input)
+
+	result := factorial(n)
+	fmt.Printf("Factorial of %d is %d\n", n, result)
+}
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+
+	result := 1
+	for i := 2; i <= n; i++ {
+		result *= i
+	}
+	return result
+}
--- src/main/java/com/test/training/go/core/live_coding_test/fibonacci/STEP_LIVE_FIBONACCI_#13.go
+++ src/main/java/com/test/training/go/core/live_coding_test/fibonacci/STEP_LIVE_FIBONACCI_#13.go
@@ -1,3 +1,36 @@
 // TODO: 피보나치
+// TODO: 피보나치 수열에서 다음 수는 앞의 두 수를 더한 값으로 이루어지는 수열
 
-package fibonacci
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	input := scanner.Text()
+
+	n, _ := strconv.Atoi(input)
+	fibonacciNumbers := fibonacci(n)
+
+	fmt.Println("Fibonacci sequence:", fibonacciNumbers)
+}
+
+func fibonacci(n int) []int {
+	if n < 2 {
+		return []int{1}
+	}
+
+	fibNums := make([]int, n)
+	fibNums[0], fibNums[1] = 1, 1
+
+	for i := 2; i < n; i++ {
+		fibNums[i] = fibNums[i-1] + fibNums[i-2]
+	}
+	return fibNums
+}
