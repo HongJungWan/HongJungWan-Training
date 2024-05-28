47af36089be3e455b0ba14dfbb316ebf319a1154
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2024-05-28 16:20:03 +0900

    feat: 뇌 단련 #215 (#214)
    Co-authored-by: jungwanhong-metanet <jungwan.hong.metanet@joyent.com>

--- src/main/java/com/test/ps/live_coding_test/string/reverse_string #1.go
+++ src/main/java/com/test/ps/live_coding_test/string/reverse_string #1.go
@@ -1,3 +1,27 @@
 // TODO: 문자열 뒤집기
 
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	input := "hello"
+	reversed := reverseString(input)
+
+	fmt.Println("Original:", input)
+	fmt.Println("Reversed:", reversed)
+}
+
+func reverseString(s string) string {
+	runes := []rune(s)
+	n := len(runes)
+
+	// 문자열을 반으로 나누어 양쪽 끝에서부터 바꾸기
+	for i := 0; i < n/2; i++ {
+		runes[i], runes[n-1-i] = runes[n-1-i], runes[i]
+	}
+
+	return string(runes)
+}
