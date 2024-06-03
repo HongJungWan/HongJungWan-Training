548d369bf1d5d4c115a61907a17e7d4190520de4
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2024-06-04 08:54:21 +0900

    feat: 뇌 단련 #222 (#221)
    Co-authored-by: jungwanhong-metanet <jungwan.hong.metanet@joyent.com>

--- src/main/java/com/test/ps/live_coding_test/graph/bfs/bfs #1.go
+++ src/main/java/com/test/ps/live_coding_test/graph/bfs/bfs #1.go
@@ -1,3 +1,67 @@
 // TODO: BFS (기초)
+// TODO: 인접 리스트 방식으로 구현, 큐를 사용한 방식
 
-package bfs
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+func BFS(graph map[int][]int, start int) {
+	// 큐 생성
+	queue := list.New()
+
+	// 방문한 노드를 기록하기 위한 맵 생성
+	visited := make(map[int]bool)
+
+	// 시작 노드를 큐에 추가하고 방문 처리
+	queue.PushBack(start)
+	visited[start] = true
+
+	// 큐가 빌 때까지 반복
+	for queue.Len() > 0 {
+		// 큐의 앞에서 노드를 꺼냅니다.
+		element := queue.Front()
+		node := element.Value.(int)
+		queue.Remove(element)
+		fmt.Println(node)
+
+		// 현재 노드의 이웃 노드들을 순회
+		for _, neighbor := range graph[node] {
+			// 방문하지 않은 이웃 노드를 큐에 추가하고 방문 처리
+			if !visited[neighbor] {
+				queue.PushBack(neighbor)
+				visited[neighbor] = true
+			}
+		}
+	}
+}
+
+func main() {
+	// 그래프를 인접 리스트로 표현
+	graph := map[int][]int{
+		0: {1, 2},
+		1: {2},
+		2: {0, 3},
+		3: {3},
+	}
+
+	// BFS 탐색 시작
+	BFS(graph, 2)
+}
+
+/*
+인접 리스트: 간선이 노드 수에 비해 적은 경우 사용 (희소 그래프)
+인접 행렬: 간선이 노드 수에 비해 많은 경우 사용 (밀집 그래프)
+
+
+container/list 패키지는 Go 표준 라이브러리 중 하나로, 양방향 연결 리스트(Doubly Linked List)를 제공하는 패키지
+
+queue := list.New(), list.New()를 호출하여 새로운 양방향 연결 리스트를 생성
+queue.PushBack(start), PushBack 메서드를 사용하여 큐의 끝부분에 새로운 요소를 추가
+
+element := queue.Front(), Front 메서드를 사용하여 큐의 앞에 있는 요소를 가져오고
+node := element.Value.(int), 그 값을 Value 필드를 통해 가져와 타입 변환
+queue.Remove(element), 그 후 Remove 메서드를 사용하여 큐에서 해당 요소를 제거
+*/
--- src/main/java/com/test/ps/live_coding_test/graph/dfs/dfs #1.go
+++ src/main/java/com/test/ps/live_coding_test/graph/dfs/dfs #1.go
@@ -1,3 +1,40 @@
 // TODO: DFS (기초)
+// TODO: 인접 리스트 방식으로 구현, 재귀를 사용한 방식
 
-package dfs
+package main
+
+import "fmt"
+
+func DFS(graph map[int][]int, start int, visited map[int]bool) {
+	// 현재 노드를 방문 처리
+	visited[start] = true
+	fmt.Println(start)
+
+	// 인접 노드들을 방문
+	for _, node := range graph[start] {
+		if !visited[node] {
+			DFS(graph, node, visited)
+		}
+	}
+}
+
+func main() {
+	// 그래프를 인접 리스트로 표현
+	graph := map[int][]int{
+		0: {1, 2},
+		1: {2},
+		2: {0, 3},
+		3: {3},
+	}
+
+	// 방문 여부를 기록하는 맵
+	visited := make(map[int]bool)
+
+	// DFS 탐색 시작
+	DFS(graph, 2, visited)
+}
+
+/*
+인접 리스트: 간선이 노드 수에 비해 적은 경우 사용 (희소 그래프)
+인접 행렬: 간선이 노드 수에 비해 많은 경우 사용 (밀집 그래프)
+*/
