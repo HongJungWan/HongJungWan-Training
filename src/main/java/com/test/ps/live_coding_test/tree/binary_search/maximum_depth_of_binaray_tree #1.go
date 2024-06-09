bb145453225912978b67d4d105f70e161bf63194
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2024-06-09 13:32:36 +0900

    feat: 뇌 단련 #227 (#226)

--- src/main/java/com/test/ps/live_coding_test/tree/binary_search/maximum_depth_of_binaray_tree #1.go
+++ src/main/java/com/test/ps/live_coding_test/tree/binary_search/maximum_depth_of_binaray_tree #1.go
@@ -1,3 +1,4 @@
 // TODO: 이진 검색 트리
+// TODO: 이진 트리의 최대 깊이를 구하는 문제
 
-package binary_search
+package main
--- src/main/java/com/test/ps/live_coding_test/tree/binary_search/validate_binary_search_tree #1.go
+++ src/main/java/com/test/ps/live_coding_test/tree/binary_search/validate_binary_search_tree #1.go
@@ -1,1 +1,4 @@
-package binary_search
+// TODO: 이진 검색 트리
+// TODO: 주어진 이진 트리가 유효한 이진 탐색 트리인지 확인
+
+package main
