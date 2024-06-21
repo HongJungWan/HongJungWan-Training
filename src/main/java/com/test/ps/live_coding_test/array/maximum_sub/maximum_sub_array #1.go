4f77e9e5ce03b47d3da7cf5ff82fa53904d9643a
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2024-06-21 12:18:27 +0900

    feat: 뇌 단련 #237 (#236)

--- src/main/java/com/test/ps/live_coding_test/array/maximum_sub/maximum_sub_array #1.go
+++ src/main/java/com/test/ps/live_coding_test/array/maximum_sub/maximum_sub_array #1.go
@@ -64,4 +64,30 @@ func maxSubArray(nums []int) int {
 1. 각 테스트 케이스에 대해 배열 크기 N과 배열 요소를 읽는다.
 2. Kadane's Algorithm, 최대 부분 배열의 합을 찾는 'maxSubArray' 함수 사용
 3. 각 테스트 케이스에 대해 최대 부분 배열의 합을 출력
+
+핵심
+1. 전체 배열이 주어졌고, 우리는 부분 배열이 가질 수 있는 부분합들 중에서 최대 부분합을 구해야 한다.
+2. O(N)으로 풀기 위한 핵심은 [각각의 최대 부분합]은 [이전 최대 부분합이 반영된 결과값]이라는 것이다.
+*/
+
+/*
+input : [2, 1, -2, 3, -5]
+
+1. 초기화 : maxSum = 2, currentSum = 2 (첫 번째 요소)
+
+2. 첫 번째 반복 (i = 1, nums[i] = 1)
+2-1. currentSum이 양수이므로, currentSum += nums[i] = 2 + 1 = 3
+2-2. maxSum 업데이트: maxSum = max(3, 2) = 3
+
+3. 두 번째 반복 (i = 2, nums[i] = -2)
+3-1. currentSum이 여전히 양수이므로, currentSum += nums[i] = 3 - 2 = 1
+3-2. maxSum 업데이트: maxSum = max(3, 1) = 3
+
+4. 세 번째 반복 (`i = 3, nums[i] = 3`)
+4-1. currentSum이 양수이므로, currentSum += nums[i] = 1 + 3 = 4
+4-2. maxSum 업데이트: maxSum = max(4, 3) = 4
+
+5. 네 번째 반복 (i = 4, nums[i] = -5)
+5-1. currentSum이 여전히 양수이므로, currentSum += nums[i] = 4 - 5 = -1
+5-2. maxSum 업데이트: maxSum = max(4, -1) = 4
 */
