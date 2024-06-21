050449e24c88d204bc2df14e55a36a1b0125dabe
jungwanhong-metanet <jungwan.hong.metanet@joyent.com>
2024-06-21 12:17:49 +0900

    feat: 뇌 단련 #237

--- src/main/java/com/test/ps/live_coding_test/array/two_sum/to_sum #1.go
+++ src/main/java/com/test/ps/live_coding_test/array/two_sum/to_sum #1.go
@@ -25,16 +25,25 @@ func twoSum(nums []int, target int) []int {
 }
 
 /*
-Input: nums = [2, 7, 11, 15], target = 9
-Output: [0, 1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+[첫 번째 반복] (i = 0, num = 2)
+
+1-1. target - num 계산: 9 - 2 = 7
+1-2. numMap에서 7을 찾기 시도: 실패
+1-3. numMap에 현재 숫자 추가: numMap[2] = 0
+1-4. 현재 상태: numMap = map[int]int{2: 0}
 
 ---
 
-numMap[target-num], 맵 numMap에서 키가 target-num인 element를 조회
+[두 번째 반복] (i = 1, num = 7)
 
----
+1-1. target - num 계산: 9 - 7 = 2
+1-2. numMap에서 2을 찾기 시도: 성공 (j = 0)
+1-3. 결과 반환: [0, 1]
+1-4. twoSum 함수 종료, result 값은 [0, 1]
+*/
 
-if j, ok := numMap[target-num]; ok { ... }, Go 언어의 "comma ok" 패턴, 맵 조회 시 특정 키가 존재하는지 확인
+/*
+numMap[target-num], 맵 numMap에서 키가 target-num인 element를 조회.
 
+if j, ok := numMap[target-num]; ok { ... }, Go 언어의 "comma ok" 패턴, 맵 조회 시 특정 키가 존재하는지 확인.
 */
